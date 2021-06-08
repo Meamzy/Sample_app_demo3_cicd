ff85df9f0db735efffe2ba5bea3ac7136e33d9b2
Mea <mumzyhdyra@gmail.com>
2021-06-08 21:42:07 +0300

    Edited ansible-playbook.yml, and main.tf

--- main.tf
+++ main.tf
@@ -52,12 +52,12 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
-  user_data = <<EOT
-#!/bin/bash
-sudo curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
-sudo docker login --username meamzy --password ~7MSim-yutv#6E7
-sudo docker run -p 2000:2000 -d meamzy/meazmysampleappcicd:latest
-EOT
+#   user_data = <<EOT
+# #!/bin/bash
+# sudo curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+# sudo docker login --username meamzy --password ~7MSim-yutv#6E7
+# sudo docker run -p 2000:2000 -d meamzy/meazmysampleappcicd:latest
+# EOT
   key_name  = aws_key_pair.deployer.key_name
   tags = {
     Name = var.instance_name
