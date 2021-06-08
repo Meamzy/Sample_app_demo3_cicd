2587c92d9ad6f573d05bb6d3a106be9272541c59
Mea <mumzyhdyra@gmail.com>
2021-06-08 21:33:34 +0300

    Added ansible-playbook, modified Terraform and .gitignore

--- variables.tf
+++ variables.tf
@@ -3,3 +3,18 @@ variable "instance_name" {
   type        = string
   default     = "CICDDemoInstance"
 }
+
+variable "INSTANCE_SSH_PUBLIC_KEY" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+}
+
+variable "default_subnet" {
+  type    = string
+  default = "subnet-c02464e1"
+}
+
+variable "alloc_id" {
+  type    = string
+  default = "eipalloc-09fc8e792c9619457"
+}
