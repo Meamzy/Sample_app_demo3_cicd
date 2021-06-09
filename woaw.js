3d534e26797b88639ea1d42bdb86c029ac489d82
Mea <mumzyhdyra@gmail.com>
2021-06-09 15:07:46 +0300

    testing master

--- woaw.js
+++ woaw.js
@@ -1,5 +1,16 @@
-so
-im gonna
-test 
-something
-at the sonarcube
+
+equ = 25
+
+return Null 
+
+
+equ = sdfsdfs
+
+function f() {
+    var z = 'foxes', r = 'birds';} // 2 local variables
+    m = 'fish'; // global, because it wasn't declared anywhere before
+  
+    function child() {
+      var r = 'monkeys'; // This variable is local and does not affect the "birds" r of the parent function.
+      z = 'penguins'; // Closure: Child function is able to access the variables of the parent function.
+    }
