55641afe33e72ef6a432a9336e3bedc72c2c4126
Mea <48360177+Meamzy@users.noreply.github.com>
2021-06-10 11:40:47 +0300

    Update main.tf
    Removed user-data

--- main.tf
+++ main.tf
@@ -51,13 +51,6 @@ resource "aws_eip_association" "eip_assoc" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-
-#   user_data = <<EOT
-# #!/bin/bash
-# sudo curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
-# sudo docker login --username meamzy --password ~7MSim-yutv#6E7
-# sudo docker run -p 2000:2000 -d meamzy/meazmysampleappcicd:latest
-# EOT
   key_name  = aws_key_pair.deployer.key_name
   tags = {
     Name = var.instance_name
